ee4b0da3358cad2c8c3489688e37395718faf164
Documenter.jl <documenter@juliadocs.github.io>
2024-04-17 11:25:18 +0000

    build based on cd0dde9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HistogramBinnings","category":"page"},{"location":"#HistogramBinnings","page":"Home","title":"HistogramBinnings","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HistogramBinnings.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HistogramBinnings]","category":"page"}]
+}
